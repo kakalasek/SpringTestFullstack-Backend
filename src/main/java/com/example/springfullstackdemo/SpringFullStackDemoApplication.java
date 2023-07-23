4c2b9770415291c2682173e19dfbbb7443361d09
Typicek <carotkacz333@seznam.cz>
2023-07-23 19:59:01 +0200

    Db connection established

--- src/main/java/com/example/springfullstackdemo/SpringFullStackDemoApplication.java
+++ src/main/java/com/example/springfullstackdemo/SpringFullStackDemoApplication.java
@@ -2,12 +2,14 @@ package com.example.springfullstackdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SpringFullStackDemoApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SpringFullStackDemoApplication.class, args);
     }
-
 }
