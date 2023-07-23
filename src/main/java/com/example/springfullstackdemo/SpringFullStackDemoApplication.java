711954b44fd6bbdb31e906fa05c0dc58b1715c5c
Typicek <carotkacz333@seznam.cz>
2023-07-23 15:05:29 +0200

    Initial commit

---
+++ src/main/java/com/example/springfullstackdemo/SpringFullStackDemoApplication.java
@@ -0,0 +1,13 @@
+package com.example.springfullstackdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringFullStackDemoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringFullStackDemoApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springfullstackdemo/SpringFullStackDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springfullstackdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringFullStackDemoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
